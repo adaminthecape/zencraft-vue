3bc0f40985c2341f170904b6a4ba99524bac3a05
adaminthecape <adaminthecape@gmail.com>
2025-03-10 14:41:09 +0200

    Create utils.ts
    Contains hardcoded store names, to avoid conflicts.

---
+++ src/pinia/utils.ts
@@ -0,0 +1,12 @@
+export enum PiniaStoreName
+{
+  fieldStore = 'fieldStore',
+  moduleStore = 'moduleStore',
+  pageStore = 'pageStore',
+  blockStore = 'blockStore',
+  itemDefinitionStore = 'itemDefinitionStore',
+  blockDefinitionStore = 'blockDefinitionStore',
+  accessRoleStore = 'accessRoleStore',
+  customItemStore = 'customItemStore',
+  firebaseAuthStore = 'firebaseAuthStore',
+}
