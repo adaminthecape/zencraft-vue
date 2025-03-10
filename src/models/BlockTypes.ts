a1e985f97ab1875ea63bceb6fb8e47b2289c84e7
adaminthecape <adaminthecape@gmail.com>
2025-03-10 14:32:36 +0200

    Create BlockTypes.ts
    Handles default data for available blocks on the system

---
+++ src/models/BlockTypes.ts
@@ -0,0 +1,204 @@
+import { fields } from 'adam-firebase-tools';
+
+export enum BlockType
+{
+  testBlock = 'testBlock',
+  headlessItemSearch = 'headlessItemSearch',
+  paletteTester = 'paletteTester',
+  itemAvatar = 'itemAvatar',
+  tourCard = 'tourCard',
+  contextProvider = 'contextProvider',
+  contextSelector = 'contextSelector',
+  itemInput = 'itemInput',
+  itemFieldValue = 'itemFieldValue',
+  contextVoid = 'contextVoid',
+  tableColumns = 'tableColumns',
+  fieldValueContextProvider = 'fieldValueContextProvider',
+  itemListValue = 'itemListValue',
+  formFields = 'formFields',
+  fieldInput = 'fieldInput',
+  clickableList = 'clickableList',
+  htmlContent = 'htmlContent',
+  newItemContext = 'newItemContext',
+  createNewItemAvatar = 'createNewItemAvatar',
+}
+
+export enum BlockCategory
+{
+  admin = 'admin',
+  layout = 'layout',
+  search = 'search',
+  input = 'input',
+  items = 'items',
+}
+
+export type BlockTypeConfigOpts = {
+  /** Path to the component to render */
+  componentPath?: string;
+  /** Fields which control options for blocks using this definition */
+  fields: fields.FieldData[];
+  /** Whether this Block can have child Blocks */
+  hasChildren?: boolean;
+  /** By default, only these BlockTypes will be allowed as direct children */
+  defaultAllowedChildren?: BlockType[];
+  /** If set, the AddBlockButton will be hidden when the maximum is reached */
+  maximumAllowedChildren?: number;
+  /** Hide the AddBlockButton (so the Block can override it) */
+  hideAddBlockButton?: boolean;
+  /** Any categories this block might belong to (for filtering) */
+  categories?: BlockCategory[];
+};
+
+export const blockTypeConfig: Record<BlockType, BlockTypeConfigOpts> = {
+  testBlock: {
+    fields: [],
+    categories: [BlockCategory.admin],
+  },
+  headlessItemSearch: {
+    fields: [],
+    hasChildren: true,
+    categories: [BlockCategory.search],
+    hideAddBlockButton: true,
+  },
+  paletteTester: {
+    fields: [],
+    categories: [BlockCategory.admin],
+  },
+  itemAvatar: {
+    fields: [],
+    categories: [BlockCategory.items],
+  },
+  tourCard: {
+    fields: [],
+    categories: [BlockCategory.items],
+  },
+  contextProvider: {
+    fields: [],
+    hasChildren: true,
+    categories: [BlockCategory.layout, BlockCategory.items],
+  },
+  contextSelector: {
+    fields: [],
+    hasChildren: true,
+    categories: [BlockCategory.layout, BlockCategory.items],
+    hideAddBlockButton: true,
+  },
+  itemInput: {
+    fields: [],
+    categories: [BlockCategory.input, BlockCategory.items],
+  },
+  itemFieldValue: {
+    fields: [],
+    categories: [BlockCategory.layout, BlockCategory.items],
+  },
+  contextVoid: {
+    fields: [],
+    categories: [BlockCategory.layout, BlockCategory.items],
+    hasChildren: true,
+  },
+  tableColumns: {
+    fields: [],
+    categories: [BlockCategory.layout],
+    hasChildren: true,
+    hideAddBlockButton: true,
+  },
+  fieldValueContextProvider: {
+    fields: [],
+    categories: [BlockCategory.layout, BlockCategory.items],
+    hasChildren: true,
+  },
+  itemListValue: {
+    fields: [],
+    categories: [BlockCategory.layout, BlockCategory.items],
+    hasChildren: true,
+    hideAddBlockButton: true,
+  },
+  formFields: {
+    fields: [],
+    categories: [BlockCategory.input, BlockCategory.items],
+    hideAddBlockButton: true,
+  },
+  fieldInput: {
+    defaultAllowedChildren: [],
+    fields: [],
+    hasChildren: false,
+    categories: [
+      BlockCategory.input
+    ],
+    hideAddBlockButton: true
+  },
+  clickableList: {
+    fields: [],
+    hasChildren: true,
+    categories: [
+      BlockCategory.layout,
+      BlockCategory.input
+    ],
+    hideAddBlockButton: true,
+  },
+  htmlContent: {
+    fields: [],
+    hasChildren: false,
+    categories: [
+      BlockCategory.layout,
+      BlockCategory.items
+    ],
+    hideAddBlockButton: true,
+  },
+  newItemContext: {
+    fields: [],
+    hasChildren: false,
+    categories: [
+      BlockCategory.input,
+      BlockCategory.items
+    ],
+    hideAddBlockButton: true
+  },
+  createNewItemAvatar: {
+    fields: [],
+    hasChildren: false,
+    categories: [
+      BlockCategory.input,
+      BlockCategory.items
+    ],
+    hideAddBlockButton: true
+  },
+};
+
+export const itemTypeIcons: Record<string, string | undefined> = {
+  Block: 'build',
+  Booking: 'hotel',
+  Field: 'handyman',
+  Hotel: 'house',
+  Item: 'category',
+  ItemDefinition: 'apartment',
+  Module: 'view_module',
+  Page: 'description',
+  Person: 'person',
+  Reminder: 'timer',
+  Room: 'bed',
+  Ticket: 'task',
+  Todo: 'task_alt',
+  Tour: 'route',
+  BlockDefinition: 'apartment',
+  AccessRole: 'key',
+};
+
+export const itemTypePrimaryFieldKeys: Record<string, string[]> = {
+  Block: ['title', 'id'],
+  Booking: ['internalId', 'id'],
+  Field: ['label', 'key', 'id'],
+  Hotel: ['title', 'id'],
+  Item: ['id'],
+  ItemDefinition: ['id'],
+  Module: ['title', 'id'],
+  Page: ['title', 'id'],
+  Person: ['firstName', 'id'],
+  Reminder: ['id'],
+  Room: ['id'],
+  Ticket: ['title', 'id'],
+  Todo: ['id'],
+  Tour: ['title', 'id'],
+  BlockDefinition: ['blockType', 'id'],
+  AccessRole: ['title', 'id'],
+};
