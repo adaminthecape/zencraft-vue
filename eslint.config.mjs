298059a82710e4be45912611940d9ed8bfdf574e
adaminthecape <adaminthecape@gmail.com>
2025-03-13 12:14:25 +0200

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+	{
+		ignores: [
+			'node_modules',
+			'dist',
+			'test.ts',
+			'tsup*',
+			'eslint*',
+			'.git*',
+			'.eslint*'
+		],
+	},
+	{
+		files: ['src/**/*.ts'],
+	},
+	{
+		languageOptions: {
+			parserOptions: {
+				parser: tseslint.parser
+			}
+		},
+	},
+    ...tseslint.configs.recommended,
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-expressions': 'off',
+		}
+	},
 ];
